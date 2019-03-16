38030a6c1e769fed7be21576c931682e8055a8d7
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-16 19:57:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
   elsif array.length == 2
      array.join(" and ")
    else
-      array.last.insert("and ")
+      array.last.insert(0,"and ")
      array.join(", ")
   end
 end
